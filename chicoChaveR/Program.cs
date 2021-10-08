2b53f9b3e8e6fcb4209a65c1128c169a7f2acd36
Fernando Konflanz <konflanz.info@gmail.com>
2021-10-08 18:09:45 -0300

    Update 08102021.2
    - Ajustes na estrutura

--- chicoChaveR/Program.cs
+++ chicoChaveR/Program.cs
@@ -41,6 +41,17 @@ namespace chicoChaveR
                 Thread.Sleep(15000);
             }
         }
+        static void adivinharChaves(string sh_token)
+        {
+            string[] chaves = lerArquivoChaves();
+
+            foreach (string n in chaves)
+            {
+                string chavesEncontrada = adivinharChave(n, sh_token, true);
+                Thread.Sleep(400);
+            }
+
+        }
         static string adivinharChave(string chaveInformada, string sh_token, bool gravarRetorno = false)
         {
             string chaveNormal = verificarEmissaoNormal(chaveInformada, sh_token, gravarRetorno);
@@ -53,35 +64,116 @@ namespace chicoChaveR
 
                 chaveEncontrada = chaveNormal;
 
-                if (chaveEncontrada != "") { Console.WriteLine("Chico ChaveR descobriu a chave: " + chaveEncontrada); }
+                Console.WriteLine("Chico ChaveR descobriu a chave: " + chaveEncontrada);
+
+                if (gravarRetorno)
+                {
+                    dynamic situacao = obterSituacao(chaveEncontrada, sh_token);
+                    DateTime dataEmissao = Convert.ToDateTime(situacao.dhRecbto);
+                    string dataLog = dataEmissao.ToShortDateString().ToString().Replace("/", "");
+
+                    using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "autorizadas_" + dataLog + ".txt", true))
+                    {
+                        outputFile.WriteLine(chaveEncontrada + " : " + situacao);
+                    }
+                }
             }
 
-            else if (chaveContingencia != "") {
-
+            else if (chaveContingencia != "")
+            {
                 chaveEncontrada = chaveContingencia;
 
-                if (chaveEncontrada != "") { 
-                    Console.WriteLine("Chico ChaveR descobriu a chave emitida em contingencia: " + chaveEncontrada); 
+                Console.WriteLine("Chico ChaveR descobriu a chave: " + chaveEncontrada);
+
+                if (gravarRetorno)
+                {
+                    dynamic situacao = obterSituacao(chaveEncontrada, sh_token);
+                    DateTime dataEmissao = Convert.ToDateTime(situacao.dhRecbto);
+                    string dataLog = dataEmissao.ToShortDateString().ToString().Replace("/", "");
+
+                    using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "autorizadas_" + dataLog + ".txt", true))
+                    {
+                        outputFile.WriteLine(chaveEncontrada + " : " + situacao);
+                    }
                 }
             }
 
             else
             {
-                if (chaveEncontrada == "") { Console.WriteLine("Chico ChaveR não encontrou este documento"); }
+                Console.WriteLine("Chico ChaveR nao encontrou a chave: " + chaveInformada);
+                if (gravarRetorno)
+                {
+                    using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "nao_autorizadas" + ".txt", true))
+                    {
+                        outputFile.WriteLine(chaveInformada);
+                    }
+                }
             }
 
             return chaveEncontrada;
         }
-        static void adivinharChaves(string sh_token)
+        private static string verificarEmissaoNormal(string chave, string sh_token, bool gravarRetorno = false)
         {
-            string[] chaves = lerArquivoChaves();
+            string chave_verificar = chave.Remove(chave.Length - 9, 9);
+            chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
+            chave_verificar = chave_verificar + "1";
+            chave_verificar = chave_verificar + gerarCodigoCDF();
+            chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
 
-            foreach(string n in chaves)
+            string chaveEmissaoNormal = obterChave(chave_verificar, sh_token, gravarRetorno);
+            return chaveEmissaoNormal;
+
+        }
+        private static string verificarEmissaoContingencia(string chave, string sh_token, bool gravarRetorno = false)
+        {
+            string chave_verificar = chave.Remove(chave.Length - 9, 9);
+            chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
+            chave_verificar = chave_verificar + "9";
+            chave_verificar = chave_verificar + gerarCodigoCDF();
+            chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
+
+            string chaveContingencia = obterChave(chave_verificar, sh_token, gravarRetorno);
+            return chaveContingencia;
+        }
+        private static string obterChave(string chave, string sh_token, bool gravarRetorno = false)
+        {
+            string cnpj = getCNPJ(chave);
+
+            ConsSitReq consultarSituacao = new ConsSitReq
             {
-                string chavesEncontrada = adivinharChave(n, sh_token, true);
-                Thread.Sleep(400);
-            }
+                tpAmb = "1",
+                chNFe = chave,
+                licencaCnpj = cnpj,
+                versao = "4.00",
+            };
 
+            string retorno = consultarSituacaoDocumento(obterMod(consultarSituacao.chNFe), consultarSituacao, sh_token);
+
+            dynamic respostaJson = JsonConvert.DeserializeObject(retorno);
+            dynamic nfeProc = respostaJson.nfeProc;
+            string xMotivo = nfeProc.xMotivo;
+            string chaveEncontrada = new String(xMotivo.Where(Char.IsDigit).ToArray());
+
+            return chaveEncontrada;
+        }
+        private static dynamic obterSituacao(string chave, string sh_token)
+        {
+            string cnpj = getCNPJ(chave);
+
+            ConsSitReq consultarSituacao = new ConsSitReq
+            {
+                tpAmb = "1",
+                chNFe = chave,
+                licencaCnpj = cnpj,
+                versao = "4.00",
+            };
+
+            string retorno = consultarSituacaoDocumento(obterMod(consultarSituacao.chNFe), consultarSituacao, sh_token);
+
+            dynamic respostaJson = JsonConvert.DeserializeObject(retorno);
+            dynamic nfeProc = respostaJson.nfeProc;
+
+            return nfeProc;
         }
         public static int gerarCodigoCDF()
         {
@@ -154,179 +246,6 @@ namespace chicoChaveR
             
         }
 
-        //private static void superChico(string[] chaves, string token)
-        //{
-        //    for(int i = 0; i <= chaves.Length; i++)
-        //    {
-        //        string chave = chaves[i].Remove(chaves[i].Length-9, 9);
-
-        //        chave = chave + gerarCodigoCDF();
-        //        chave = chave + GerarDigitoVerificador(chave);
-
-        //        string cnpj = chaves[i].Remove(chaves[i].Length - 24, 24);
-        //        cnpj = cnpj.Remove(0, 6);
-
-        //        ConsSitReq ConsSitReq = new ConsSitReq
-        //        {
-        //            tpAmb = "1",
-        //            chNFe = chave,
-        //            licencaCnpj = cnpj,
-        //            versao = "4.00",
-        //        };
-
-        //        Thread.Sleep(500);
-        //        string retorno = consultarSituacaoDocumento("65", ConsSitReq, token);
-                
-        //        dynamic respostaJson = JsonConvert.DeserializeObject(retorno);
-        //        dynamic nfeProc = respostaJson.nfeProc;
-        //        string xMotivo = nfeProc.xMotivo;
-        //        string chaveEncontrada = new String(xMotivo.Where(Char.IsDigit).ToArray());
-
-        //        if (chaveEncontrada != "") { Console.WriteLine("Chico ChaveR descobriu a chave: " + chaveEncontrada); }
-        //        else { Console.WriteLine("Chico ChaveR não encontrou a chave "+ chave + ": " + xMotivo); }
-
-        //        if (chaveEncontrada != "")
-        //        {
-        //            ConsSitReq segundaReq = new ConsSitReq
-        //            {
-        //                tpAmb = "1",
-        //                chNFe = chaveEncontrada,
-        //                licencaCnpj = cnpj,
-        //                versao = "4.00",
-        //            };
-
-        //            Thread.Sleep(500);
-        //            string segundoRetorno = consultarSituacaoDocumento("65", segundaReq, token);
-
-        //            dynamic respostaJson2 = JsonConvert.DeserializeObject(segundoRetorno);
-        //            dynamic nfeProc2 = respostaJson2.nfeProc;
-        //            string status = respostaJson2.status; if (status == "-997")
-        //            {
-        //                using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "retorno" + ".txt", true))
-        //                {
-        //                    outputFile.WriteLine(chaveEncontrada + " : " + segundoRetorno);
-        //                }
-        //            }
-        //            if (status != "-997")
-        //            {
-        //                string cStat = nfeProc2.cStat;
-        //                if (cStat == "217")
-        //                {
-        //                    string chave_verificar = chaves[i].Remove(chaves[i].Length - 9, 9);
-
-        //                    if (chave_verificar.EndsWith("9"))
-        //                    {
-        //                        chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
-        //                        chave_verificar = chave_verificar + "1";
-        //                        chave_verificar = chave_verificar + gerarCodigoCDF();
-        //                        chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
-
-        //                        ConsSitReq verificar_tp_emis = new ConsSitReq
-        //                        {
-        //                            tpAmb = "1",
-        //                            chNFe = chave_verificar,
-        //                            licencaCnpj = cnpj,
-        //                            versao = "4.00",
-        //                        };
-
-        //                        Thread.Sleep(500);
-        //                        string retorno_verificar = consultarSituacaoDocumento("65", verificar_tp_emis, token);
-        //                        dynamic respostaJson_verificar = JsonConvert.DeserializeObject(retorno_verificar);
-        //                        dynamic nfeProc_verificar = respostaJson_verificar.nfeProc;
-
-        //                        string xMotivo_verificar = nfeProc_verificar.xMotivo;
-        //                        string chave_reconsultar = new String(xMotivo_verificar.Where(Char.IsDigit).ToArray());
-
-        //                        if (chave_reconsultar != "") { Console.WriteLine("Chico ChaveR descobriu a chave: " + chave_reconsultar); }
-        //                        else { Console.WriteLine("Chico ChaveR não encontrou a chave: " + nfeProc_verificar.xMotivo); }
-
-        //                        ConsSitReq reconsulta = new ConsSitReq
-        //                        {
-        //                            tpAmb = "1",
-        //                            chNFe = chave_reconsultar,
-        //                            licencaCnpj = cnpj,
-        //                            versao = "4.00",
-        //                        };
-
-        //                        Thread.Sleep(500);
-        //                        string retorno_reconsulta = consultarSituacaoDocumento("65", reconsulta, token);
-
-        //                        string cStat_verificar = nfeProc_verificar.cStat;
-        //                        string status_verificar = respostaJson_verificar.status;
-
-        //                        using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "retorno" + ".txt", true))
-        //                        {
-        //                            outputFile.WriteLine(chave_reconsultar + " : " + retorno_reconsulta);
-        //                        }
-        //                    }
-        //                    else
-        //                    {
-        //                        chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
-        //                        chave_verificar = chave_verificar + "9";
-        //                        chave_verificar = chave_verificar + gerarCodigoCDF();
-        //                        chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
-
-        //                        ConsSitReq verificar_tp_emis = new ConsSitReq
-        //                        {
-        //                            tpAmb = "1",
-        //                            chNFe = chave_verificar,
-        //                            licencaCnpj = cnpj,
-        //                            versao = "4.00",
-        //                        };
-
-        //                        Thread.Sleep(500);
-        //                        string retorno_verificar = consultarSituacaoDocumento("65", verificar_tp_emis, token);
-        //                        dynamic respostaJson_verificar = JsonConvert.DeserializeObject(retorno_verificar);
-        //                        dynamic nfeProc_verificar = respostaJson_verificar.nfeProc;
-
-        //                        string xMotivo_verificar = nfeProc_verificar.xMotivo;
-        //                        string chave_reconsultar = new String(xMotivo_verificar.Where(Char.IsDigit).ToArray());
-
-        //                        if (chave_reconsultar != "") { Console.WriteLine("Chico ChaveR descobriu a chave: " + chave_reconsultar); }
-        //                        else { Console.WriteLine("Chico ChaveR não encontrou a chave: " + nfeProc_verificar.xMotivo); }
-
-        //                        ConsSitReq reconsulta = new ConsSitReq
-        //                        {
-        //                            tpAmb = "1",
-        //                            chNFe = chave_reconsultar,
-        //                            licencaCnpj = cnpj,
-        //                            versao = "4.00",
-        //                        };
-
-        //                        Thread.Sleep(500);
-        //                        string retorno_reconsulta = consultarSituacaoDocumento("65", reconsulta, token);
-
-        //                        string cStat_verificar = nfeProc_verificar.cStat;
-        //                        string status_verificar = respostaJson_verificar.status;
-
-
-        //                        using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "retorno" + ".txt", true))
-        //                        {
-        //                            outputFile.WriteLine(chave_reconsultar + " : " + retorno_reconsulta);
-        //                        }
-
-        //                    }
-        //                }
-
-        //                else
-        //                {
-        //                    using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "retorno" + ".txt", true))
-        //                    {
-        //                        outputFile.WriteLine(nfeProc2.chNFe + " : " + segundoRetorno);
-        //                    }
-        //                }
-        //            }
-        //        }
-        //        else
-        //        {
-        //            using (StreamWriter outputFile = new StreamWriter(@"./ret/" + "retorno" + ".txt", true))
-        //            {
-        //                outputFile.WriteLine(chave + " : " + xMotivo);
-        //            }
-        //        }
-        //    }
-        //}
-
         private static string gerarNovaChave(string chave)
         {
             string novaChave = chave.Remove(chave.Length - 9, 9);
@@ -342,84 +261,15 @@ namespace chicoChaveR
 
             return cnpj;
         }
-        private static string obterChave(string chave, string sh_token, bool gravarRetorno = false)
+
+        private static string obterMod(string chave)
         {
-            string cnpj = getCNPJ(chave);
+            string mod = chave.Remove(chave.Length - 22, 22);
+            mod = mod.Remove(0, 20);
 
-            ConsSitReq consultarSituacao = new ConsSitReq
-            {
-                tpAmb = "1",
-                chNFe = chave,
-                licencaCnpj = cnpj,
-                versao = "4.00",
-            };
-
-            string retorno = consultarSituacaoDocumento("65", consultarSituacao, sh_token);
-
-            dynamic respostaJson = JsonConvert.DeserializeObject(retorno);
-            dynamic nfeProc = respostaJson.nfeProc;
-            string xMotivo = nfeProc.xMotivo;
-            string chaveEncontrada = new String(xMotivo.Where(Char.IsDigit).ToArray());
-
-            dynamic situacao = "";
-
-            if (chaveEncontrada != "")
-            {
-                situacao = obterSituacao(chaveEncontrada, sh_token);
-
-                if (gravarRetorno)
-                {
-                    using (StreamWriter outputFile = new StreamWriter(@"./" + "consultas_"+ cnpj + ".txt", true))
-                    {
-                        outputFile.WriteLine(chaveEncontrada + " : " + situacao);
-                    }
-                }
-            }
-
-            return chaveEncontrada;
+            return mod;
         }
-        private static dynamic obterSituacao(string chave, string sh_token)
-        {
-            string cnpj = getCNPJ(chave);
 
-            ConsSitReq consultarSituacao = new ConsSitReq
-            {
-                tpAmb = "1",
-                chNFe = chave,
-                licencaCnpj = cnpj,
-                versao = "4.00",
-            };
-
-            string retorno = consultarSituacaoDocumento("65", consultarSituacao, sh_token);
-
-            dynamic respostaJson = JsonConvert.DeserializeObject(retorno);
-            dynamic nfeProc = respostaJson.nfeProc;
-
-            return nfeProc;
-        }
-        private static string verificarEmissaoNormal(string chave, string sh_token, bool gravarRetorno = false)
-        {
-            string chave_verificar = chave.Remove(chave.Length - 9, 9);
-            chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
-            chave_verificar = chave_verificar + "1";
-            chave_verificar = chave_verificar + gerarCodigoCDF();
-            chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
-
-            string chaveEmissaoNormal = obterChave(chave_verificar, sh_token, gravarRetorno);
-            return chaveEmissaoNormal;  
-
-        }
-        private static string verificarEmissaoContingencia(string chave, string sh_token, bool gravarRetorno = false)
-        {
-            string chave_verificar = chave.Remove(chave.Length - 9, 9);
-            chave_verificar = chave_verificar.Remove(chave_verificar.Length - 1, 1);
-            chave_verificar = chave_verificar + "9";
-            chave_verificar = chave_verificar + gerarCodigoCDF();
-            chave_verificar = chave_verificar + GerarDigitoVerificador(chave_verificar);
-
-            string chaveContingencia = obterChave(chave_verificar, sh_token, gravarRetorno);
-            return chaveContingencia;
-        }
         public class ConsSitReq
         {
             public string licencaCnpj { get; set; }
